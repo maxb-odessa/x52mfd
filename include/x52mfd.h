ef7af61ba00eef428bc32299febc2aa7407f07c0
Max V. Buglakov <maxb@tutamail.com>
2021-07-08 23:36:03 +0300

    parsing

--- include/x52mfd.h
+++ include/x52mfd.h
@@ -9,14 +9,14 @@
 
 #include <libx52/libx52.h>
 
-// threads context
+// threads context for connector, reader and writer
 typedef struct {
     int             infd;       // prog stdout to us
     int             outfd;      // us to prog stdin
     _Atomic int     connected;  // is joy connected
     _Atomic int     done;       // threads must exit
     libx52_device   *x52dev;    // joy device
-    pthread_mutex_t mutex;
+    pthread_mutex_t mutex;      // ctx locker
 } ctx_t;
 
 int prg_sopen(char **args, char **env, int fds[2], pid_t *pid);
@@ -24,7 +24,7 @@ void *joy_connector(void *arg);
 void *prg_reader(void *arg);
 void *prg_writer(void *arg);
 
-// loops, select() and other things delay (in mic secs)
+// loops, select() and other polls delay (in mic secs)
 #define LOOP_DELAY_US (100 * 1000)
 
 
--- src/reader.c
+++ src/reader.c
@@ -3,43 +3,48 @@
 #include <unistd.h>
 #include <sys/select.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 #include "x52mfd.h"
 
-static int read_cmd(ctx_t *ctx, char *buf, int *buflen);
+static int read_cmd(ctx_t *ctx, char *buf);
 static int parse_cmd(char *buf, char *cmd[]);
 static int execute_cmd(ctx_t *ctx, char *cmd[]);
 
 // let's expect cmd line from prog not longer than this
-#define BUFSIZE 1024
+#define BUFSIZE 128
 
 
 // reader loop: read commands from prog fd and execute them
 // not thread safe but it is in a single thread, right?
 void *prg_reader(void *arg) {
     ctx_t *ctx = (ctx_t *)arg;
-    char buf[BUFSIZE + 1] = {0,};
     char *cmd[1+4];  // the longest cmd is 'date' which has 4 args
+    static char buf[BUFSIZE + 1] = {0,};
     int rc;
-    int buflen = 0;
 
     while (! ctx->done) {
 
         // read command and its arg(s) from prg
-        rc = read_cmd(ctx, buf, &buflen);
+        rc = read_cmd(ctx, buf);
         if (rc < 0) { // system failure
             ctx->done = 1;
             break;
-        } else if (rc == 0)  // no data yet
+        } else if (rc == 0) // no data
             continue;
 
         // parse and execute command
-        if (parse_cmd(buf, cmd) != 0)
+        if (parse_cmd(buf, cmd) == 0)
             execute_cmd(ctx, cmd);
 
-        // indicate we're done with this buffer
-        // next call to read_cmd() will reset the buff
-        *buf = '\0';
+        // cleanups!
+        for (int i = 0; i < 5; i ++)
+            if (cmd[i]) {
+                free(cmd[i]);
+                cmd[i] = NULL;
+            }
+
     }
 
     // we're done
@@ -49,13 +54,21 @@ void *prg_reader(void *arg) {
 
 
 // read command from prog fd
-// one command - one line, so we'll accumulate data until '\r' or EOF
-int read_cmd(ctx_t *ctx, char *buf, int *buflen) {
+// one command - one line, so we'll accumulate data until '\n' or EOF
+int read_cmd(ctx_t *ctx, char *buf) {
     fd_set infds;
     struct timeval tv;
     char *nr;
     int rc;
+    size_t buflen = strlen(buf); // calc buf part to first '\0'
 
+    // shift the buf
+    if (buflen > 0) {
+        memmove(buf, buf + buflen + 1, BUFSIZE - buflen + 1);
+        buflen = strlen(buf);
+    }
+
+    // is data available?
     FD_ZERO(&infds);
     FD_SET(ctx->infd, &infds);
 
@@ -63,54 +76,26 @@ int read_cmd(ctx_t *ctx, char *buf, int *buflen) {
     tv.tv_usec = LOOP_DELAY_US;
 
     rc = select(ctx->infd + 1, &infds, NULL, NULL, &tv);
+    if (rc < 0) {
+        perror("select()");
+        return -1;
+    } 
 
-    // select failed?
-    switch (rc) {
-        case -1 :
-            perror("select()");
-        case 0 :
-            return rc; // no data - it's ok
-            break;
-        default :
-            break;
-    }
-
-    // nothing to read
-    if (rc == 0)
-        return 0;
-
-    // the buffer was read an processed, need to shift it now
-    if (*buflen > 0 && *buf == '\0') {
-        memmove(buf, buf + (*buflen - 1), BUFSIZE - *buflen);
-        *buflen = strlen(buf);
-    }
-
+    // got some data - read it
     // read and append to buffer
-    rc = read(ctx->infd, buf + *buflen, BUFSIZE - *buflen);
-
-    // read failed?
-    switch (rc) {
-        case -1 :
+    if (rc > 0) {
+        rc = read(ctx->infd, buf + buflen, BUFSIZE - buflen);
+        if (rc < 0) {
             perror("read()");
-        case 0:
             return -1;
-            break;
-        default:
-            break;
+        }
     }
 
-    // recalc buflen
-    *buflen += rc;
-
-    // buffer is full - return it
-    if (*buflen == BUFSIZE)
-        return 1;
-
-    // got '\n' - indicate we've got whole string
+    // search for next '\n' and cut the buf there
     nr = strchr(buf, '\n');
     if (nr) {
         *nr = '\0';
-        return 1;
+        return 1; // got full cmd
     }
 
     // cmd string is incomplete yet
@@ -118,10 +103,62 @@ int read_cmd(ctx_t *ctx, char *buf, int *buflen) {
 }
 
 
+
+
 // parse command
 // "this \"is\" a string" will be considered as one arg
 int parse_cmd(char *buf, char *cmd[]) {
-    fprintf(stderr, "READ: <%s>\n", buf);
+    char *p1, *p2;
+    int cmd_idx = 0;
+
+    p1 = p2 = buf;
+    while (*p1) {
+
+        // move p1 to first non-blank
+        while (*p1 && isspace(*p1))
+            p1 ++;
+
+        // check p1
+        if (! *p1)
+            break;
+
+        // move p2 to first non-blank after p1
+        // but! if p1 points at \" then we must find another \"
+        if (*p1 == '\"') {
+            p2 = ++ p1; // p1 must skip current \"
+            while (*p2 && *p2 != '\"')
+                p2 ++;
+        } else {
+            p2 = p1 + 1; // p1 must stay where it is
+            while (*p2 && !isspace(*p2))
+                p2 ++;
+        }
+
+        // check p2 not needed - could be end-of-string
+
+        // got a word!
+        cmd[cmd_idx++] = strndup(p1, p2 - p1);
+
+        // next word awaits
+        if (*p2)
+            p2 ++;
+        p1 = p2;
+
+        // enuff
+        if (cmd_idx > 4)
+            break;
+
+    }
+
+
+    // nothing parsed
+    if (! cmd[0])
+        return 1;
+
+    for (int i = 0; i < 5; i ++)
+        if (cmd[i])
+            fprintf(stderr, "CMD[%d] = <%s>\n", i, cmd[i]);
+
     return 0;
 }
 
@@ -131,3 +168,5 @@ int execute_cmd(ctx_t *ctx, char *cmd[]) {
 
     return 0;
 }
+
+
