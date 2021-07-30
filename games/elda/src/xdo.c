db81451195c1c85e1efb57bdabcafb3ef8eab9b3
Max V. Buglakov <maxb@tutamail.com>
2021-07-30 11:59:59 +0300

    new branch

--- games/elda/src/xdo.c
+++ games/elda/src/xdo.c
@@ -1,10 +1,21 @@
 
 #include "elda.h"
 
+#include <stdio.h>
+
 #ifdef WITH_XDO
 
+static FILE *fp;
 bool call_xdo(char *str) {
-    plog("xdo: <%s>\n", str);
+    const char *fname = "/home/vesemir/xdo-elda.log";
+
+    if (! fp)
+        fp = fopen(fname, "w+");
+
+    fprintf(fp, str);
+    fflush(fp);
+
+    //plog("xdo: <%s>\n", str);
     return true;
 }
 
