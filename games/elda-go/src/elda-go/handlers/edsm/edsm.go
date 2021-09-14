0d3719c8944953778a404fcca3e534a7e1302c58
Max V. Buglakov <maxb@tutamail.com>
2021-09-14 10:51:32 +0300

    time for refactoring

--- games/elda-go/src/elda-go/handlers/edsm/edsm.go
+++ games/elda-go/src/elda-go/handlers/edsm/edsm.go
@@ -1,6 +1,10 @@
 package edsm
 
 import (
+	"bytes"
+	"fmt"
+	"mime/multipart"
+	"net/http"
 	"time"
 
 	"elda-go/def"
@@ -15,7 +19,9 @@ type handler struct {
 	typ  int
 
 	// optional
-	buf string
+	url  string
+	mpw  *multipart.Writer
+	flds map[string]string
 }
 
 // register us
@@ -27,6 +33,30 @@ func Register() *handler {
 }
 
 func (self *handler) Init(vars map[string]string) error {
+	var err error
+
+	self.flds = make(map[string]string)
+
+	if self.url, err = def.GetStrVar(vars, "url"); err != nil {
+		return err
+	}
+
+	if self.flds["apiKey"], err = def.GetStrVar(vars, "api_key"); err != nil {
+		return err
+	}
+
+	if self.flds["commanderName"], err = def.GetStrVar(vars, "commander"); err != nil {
+		return err
+	}
+
+	if self.flds["fromSoftware"], err = def.GetStrVar(vars, "software_name"); err != nil {
+		return err
+	}
+
+	if self.flds["fromSoftwareVersion"], err = def.GetStrVar(vars, "software_ver"); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -38,11 +68,40 @@ func (self *handler) Type() int {
 	return self.typ
 }
 
+// for POST requests
+// https://www.edsm.net/en/api-journal-v1
 func (self *handler) Push(s string) error {
-	buf = s
+
+	body := new(bytes.Buffer)
+	mpw := multipart.NewWriter(body)
+	defer mpw.Close()
+
+	for key, val := range self.flds {
+		mpw.WriteField(key, val)
+	}
+	mpw.WriteField("message", s)
+
+	req, err := http.NewRequest(http.MethodPost, self.url, bytes.NewReader(body.Bytes()))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", mpw.FormDataContentType())
+
+	client := &http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	rsp, _ := client.Do(req)
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != http.StatusOK {
+		fmt.Errorf("POST to '%s' failed: %s", self.url, http.StatusText(rsp.StatusCode))
+	}
+
 	return nil
 }
 
+// for GET reuqests?
 func (self *handler) Pull() (string, error) {
 	time.Sleep(time.Second * 3)
 	return buf, nil
